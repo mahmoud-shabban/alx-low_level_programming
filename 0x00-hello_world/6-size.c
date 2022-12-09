49f51b476f3595f7dc28b9316c0d2d3ccb1c876e
mahmoud <mahmoudshabban300@gmail.com>
2022-12-09 16:03:34 +0200

    dsf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+	printf("Size of a char: %lx byte(s)\n", sizeof(char));
+	printf("Size of an int: %lx byte(s)\n", sizeof(int));
+ 	printf("Size of a long: %lx byte(s)\n", sizeof(long));
+    printf("Size of a long long: %lx byte(s)\n", sizeof(long long));
+    printf("Size of a float: %lx byte(s)\n", sizeof(float));
+
+	return 0;
+}
--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
 int main(void){
+	printf("%lx",sizeof(int));
 	return (0);
 }
