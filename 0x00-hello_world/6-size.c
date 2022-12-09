1f1970ebae216be6378631b30ec681ef5f5299d2
mahmoud <mahmoudshabban300@gmail.com>
2022-12-09 16:06:04 +0200

    dsf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 int main(void){
 	printf("Size of a char: %lx byte(s)\n", sizeof(char));
 	printf("Size of an int: %lx byte(s)\n", sizeof(int));
- 	printf("Size of a long: %lx byte(s)\n", sizeof(long));
+ 	printf("Size of a long int: %lx byte(s)\n", sizeof(long));
     printf("Size of a long long int: %lx byte(s)\n", sizeof(long long));
     printf("Size of a float: %lx byte(s)\n", sizeof(float));
 
