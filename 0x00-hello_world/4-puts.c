656c48320a6fc9d399d676918f3ffadc0c72a037
mahmoud <mahmoudshabban300@gmail.com>
2022-12-09 16:34:01 +0200

    dsf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main(void){
+/**
+ * main - tes docs
+ * Return: inte
+ */
 
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
