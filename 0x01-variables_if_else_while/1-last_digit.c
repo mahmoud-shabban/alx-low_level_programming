019f1fe62ed10ef316f6c742aa0fbd82cf618d29
mahmoud <mahmoudshabban300@gmail.com>
2022-12-12 22:13:35 +0200

    sasf

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n, last_d;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	last_d = n % 10;
+	if (last_d > 5)
+	{
+		printf("%d is positive", last_d);
+	}
+	else if (last_d == 0)
+	{
+		printf("%d is zero", last_d); 
+	}
+	else if (last_d < 0)
+	{
+		printf("%d is negative", last_d);
+	}
+
+	return (0);
+}
