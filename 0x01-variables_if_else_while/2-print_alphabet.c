5becb46511b9bb82c5c4a153ac8f845d7465ecc9
mahmoud <mahmoudshabban300@gmail.com>
2022-12-12 23:14:07 +0200

    sasf

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - tes docs
+ * Return: inte
+ */
+/* more headers goes there */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
