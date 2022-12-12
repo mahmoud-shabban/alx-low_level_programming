5922f12334ea9e3d4ade22f39c76aaaebf113049
mahmoud <mahmoudshabban300@gmail.com>
2022-12-12 23:46:33 +0200

    sasf

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,15 +11,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 48; i < 57; i++)
 	{
 		putchar(i);
-	}
-	if (i != 57)
-	{
 		putchar(',');
 		putchar(' ');
 	}
+	putchar(57);
 	putchar('\n');
 
 	return (0);
