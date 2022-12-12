8f7c9dafaed7099cb25dd1710cbc543002d4586c
mahmoud <mahmoudshabban300@gmail.com>
2022-12-12 23:02:17 +0200

    sasf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ int main(void)
 	last_d = n % 10;
 	if (last_d > 5)
 	{
-		printf("Last digit of %d and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
 	}
 	else if (last_d == 0)
 	{
-		printf("Last digit of %d is zero\n", n);
+		printf("Last digit of %d is %d and is zero\n", n, last_d);
 	}
-	else if (last_d < 0)
+	else if (last_d < 6 && last_d != 0)
 	{
-		printf("Last digit of %d is negative\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
 	}
 
 	return (0);
