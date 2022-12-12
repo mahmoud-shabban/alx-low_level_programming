b404e4f09831bc3e5989a632efc4a33fe89b8c4a
mahmoud <mahmoudshabban300@gmail.com>
2022-12-12 23:39:12 +0200

    sasf

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - tes docs
+ * Return: inte
+ */
+/* more headers goes there */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
