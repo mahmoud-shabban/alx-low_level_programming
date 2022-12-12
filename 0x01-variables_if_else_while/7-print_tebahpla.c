4327095ffd08eb265b8634c3a9111d9dbbf4f67c
mahmoud <mahmoudshabban300@gmail.com>
2022-12-12 23:32:05 +0200

    sasf

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - tes docs
+ * Return: inte
+ */
+/* more headers goes there */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
