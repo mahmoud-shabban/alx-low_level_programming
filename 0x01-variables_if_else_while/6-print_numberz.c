34cdbcd115238381b6a7cbed901164760fdb4f9d
mahmoud <mahmoudshabban300@gmail.com>
2022-12-12 23:29:43 +0200

    sasf

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	char ch;
+	int ch;
 
-	for (ch = '0'; ch <= '9'; ch++)
+	for (ch = 0; ch <= 9; ch++)
 	{
-		putchar(ch);
+		putchar(ch%10 + '0');
 	}
 	putchar('\n');
 	return (0);
