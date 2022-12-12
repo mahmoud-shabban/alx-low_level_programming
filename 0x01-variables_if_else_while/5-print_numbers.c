b18e90efc3b320ecd22db4d5a55aa8a6cb9a58c2
mahmoud <mahmoudshabban300@gmail.com>
2022-12-12 23:21:45 +0200

    sasf

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - tes docs
+ * Return: inte
+ */
+/* more headers goes there */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		printf("%d",ch);
+	}
+	putchar('\n');
+	return (0);
+}
