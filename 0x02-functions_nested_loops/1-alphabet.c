afad161eb851817177ff52d96e5daa41813894fb
mahmoud <mahmoudshabban300@gmail.com>
2022-12-22 13:34:16 +0200

    sasf

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,6 @@
 
 int main(void)
 {
-	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		_putchar(ch);
-	}
-	_putchar('\n');
+	print_alphabet();
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,14 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+void print_alphabet(void)
+{
+	char ch;
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+                _putchar(ch);
+        }
+        _putchar('\n');
+
+}
