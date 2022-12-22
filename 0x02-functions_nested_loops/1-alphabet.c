79060986f90adf1c5c5c6c1341f7065fce76d4b6
mahmoud <mahmoudshabban300@gmail.com>
2022-12-22 14:50:35 +0200

    sasf

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
+
 /**
  * main - tes docs
  * Return: inte
  */
 /* more headers goes there */
 
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char c;
+	for(c='a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
 }
