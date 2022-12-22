9e356f8d5ce54d22663ce401d4eb4c3b24a6cdd9
mahmoud <mahmoudshabban300@gmail.com>
2022-12-22 16:22:58 +0200

    sasf

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - tes docs
+ * @c: int
+ * Return: inte
+ */
+/* more headers goes there */
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	return (1);
+	else
+	return (0);
+}
